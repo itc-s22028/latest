85e839f3baf6402222497af6cca4a4dd1eb0eb88
itc-s22028 <s22028@std.it-college.ac.jp>
2023-02-15 16:01:26 +0000

    chap5 fin

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,15 @@
 import Container from 'components/container'
 import Logo from 'components/logo'
+import Social from 'components/social'
 import styles from 'styles/footer.module.css'
 
-export default function Footer () {
+export default function Footer() {
   return (
     <footer className={styles.wrapper}>
       <Container>
         <div className={styles.flexContainer}>
           <Logo />
-          [ソーシャル]
+          <Social />
         </div>
       </Container>
     </footer>
