9492a320b5e0a97dd7828bd24deccfcaa2416025
itc-s22028 <s22028@std.it-college.ac.jp>
2023-02-19 08:19:20 +0000

    9.6

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,10 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
 
 export default function Blog ({ posts }) {
   return (
@@ -19,6 +23,13 @@ export default function Blog ({ posts }) {
 export async function getStaticProps () {
   const posts = await getAllPosts()
 
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
   return {
     props: {
       posts,
